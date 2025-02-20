a01708a853d1d203a154ff771f84cc2502d4d717
AJ Ashik <147678348+ashik2770@users.noreply.github.com>
2025-02-21 00:06:09 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#141414',
+        secondary: '#e50914',
+      },
+    },
+  },
+  plugins: [],
+};
